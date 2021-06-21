a6dc3eb985301e1fdef84d703ed13efdb288c709
seongmin <tjdalsdl0219@gmail.com>
2021-06-21 23:54:46 +0900

    프로그래머스 K번째 수 , 제출 채점 모두 실패

--- Knumber.py
+++ Knumber.py
@@ -1,9 +1,13 @@
 if __name__ == '__main__':
 
+
+
     import copy
 
 
     def solution(array, commands):
+
+        #print문으로 확인 자제ㄷ
 
         answer = []
         origin_array = copy.deepcopy(array)
@@ -11,8 +15,10 @@ if __name__ == '__main__':
             command = commands[i]
             left_idx = commands[i][0]
             right_idx = commands[i][1]
-            temp_arr = array[left_idx:right_idx]
+            temp_arr = array[left_idx - 1:right_idx]
             sorted_array = sorted(temp_arr)
-            print(command)
-            answer.append(sorted_array[command[1]])
+            print(command[2])
+            print(sorted_array)
+            answer.append(sorted_array[command[2] - 1])
+            print(answer)
         return answer
