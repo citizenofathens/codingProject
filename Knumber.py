73fbb91a66f27bcf267c2ce0f0e804e926780327
seongmin <tjdalsdl0219@gmail.com>
2021-06-23 00:32:24 +0900

    프로그래머스 K번째 수 , 제출 채점 모두 실패 -> 다른 사람 풀이 참고

--- Knumber.py
+++ Knumber.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
 
 
-
+    # 최초 작성 코드  . 최악은 피하자
     import copy
 
 
@@ -21,4 +21,29 @@ if __name__ == '__main__':
             print(sorted_array)
             answer.append(sorted_array[command[2] - 1])
             print(answer)
+        return answer
+
+
+    import copy
+
+
+    # 런타임 오류가 나서 다른 사람 질문 참고
+    def solution(array, commands):
+
+        answer = []
+
+        temp_arr = []
+        # 이런 식으로 했을 때 런타임 오류 난다 이유는 ?
+        # for i in range(3):
+        #   left_idx = command[i][0]
+        #   right_idx = command[i][1]
+        #   temp_arr = array[left_idx-1:right_idx]
+
+        for command in commands:
+            # deepcopy 해서 array를 복사하지 않아도 기존의 array는 변하지 않고 temp_arr에 담기므로 copy할 필요가 없다. 어찌보면 다형성
+
+            temp_arr = array[command[0] - 1:command[1]]
+            temp_arr.sort()
+            answer.append(temp_arr[command[2] - 1])
+
         return answer
